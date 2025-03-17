a5a1fcacbe383d1c2693360f2381071f30a25704
cosmin-eugen.vleju <@student.tuiasi.ro>
2025-03-17 07:46:40 +0200

    first commit

---
+++ src/main/java/IasiTranzit/Tranzy_Iasi/App.java
@@ -0,0 +1,10 @@
+package IasiTranzit.Tranzy_Iasi;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
