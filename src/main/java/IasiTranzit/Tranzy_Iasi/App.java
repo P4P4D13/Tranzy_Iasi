7eb48009bc94e907dff4f30150c63d92ea6da8ee
ioana <@student.tuiasi>
2025-03-24 09:00:09 +0200

    commit 1

--- src/main/java/IasiTranzit/Tranzy_Iasi/App.java
+++ src/main/java/IasiTranzit/Tranzy_Iasi/App.java
@@ -6,6 +6,6 @@ package IasiTranzit.Tranzy_Iasi;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Ioana");
+        System.out.println("Ioana ");
     }
 }
