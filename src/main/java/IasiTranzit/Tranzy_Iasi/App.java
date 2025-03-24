54fb00d624d16fbccab1cb9d315b12c807dbbdac
cosmin-eugen.vleju <@student.tuiasi.ro>
2025-03-24 09:26:19 +0200

    stergere ui.java

--- src/main/java/IasiTranzit/Tranzy_Iasi/App.java
+++ src/main/java/IasiTranzit/Tranzy_Iasi/App.java
@@ -6,10 +6,7 @@ package IasiTranzit.Tranzy_Iasi;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-<<<<<<< HEAD
         System.out.println("Ioana ");
-=======
         System.out.println("AlexL");
->>>>>>> 08a0bc43436f62e5221f6de523d684b12d03a153
     }
 }
